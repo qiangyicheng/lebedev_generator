eb1c71fe963d844077b44f77191f9da93c846e36
Yicheng Qiang <qiangyicheng@icloud.com>
2021-08-23 23:30:20 +0800

    add a simple test for lebedev_info

--- test/lebedev_info_test.cpp
+++ test/lebedev_info_test.cpp
@@ -13,3 +13,12 @@ TEST(CArrayTest, BasicCArrayCreation) {
   constexpr c_array<int,7> test_arr_3{1,2,3,4,5};
   EXPECT_NE(test_arr, test_arr_3);
 }
+
+TEST(LebedevInfoTest, ValidRules) {
+  using lebedev::lebedev::LebedevInfo;
+  using LebedevRule15=LebedevInfo<15>;
+
+  EXPECT_EQ(LebedevRule15::precision_, 31)<<"Incorrect Precision";
+  EXPECT_EQ(LebedevRule15::if_available_, true);
+
+}
